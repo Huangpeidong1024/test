4dac4572df543c56012e66d201348cfd2ac49824
peter <721545782@qq.com>
2019-09-03 23:30:39 +0800

    first

---
+++ src/TestMain.java
@@ -0,0 +1,9 @@
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("输出：00000000000000000000000000000000000000000000000000000000000");
+	}
+
+}
